92e05a6860efd255f8197744ab0931adc9167e7a
Yan <yanbaldissera1@gmail.com>
2023-09-27 19:46:29 -0300

    Atualizações 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,33 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
 
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `tel:${profileData.phone}`
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `mailto: ${profileData.email}`
+}
+
+function updateCourses(profileData){
+    const courses = document.getElementById('profile.skills.courses')
+    courses.innerHTML = profileData.skills.courses.map(skill => `<li>${skill}</li>`).join('')
+}
 
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(profileData)
-})
+    updateProfileInfo(profileData)
+    updateCourses(profileData)
+})()
