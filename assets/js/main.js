b962b97969487008ee39d509e9cb1aa5490d5bdf
Yan <yanbaldissera1@gmail.com>
2023-09-28 09:09:36 -0300

    Atualizações 7

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,10 +36,21 @@ function updateLanguages(profileData){
     idioma.innerHTML = profileData.languages.map(languages => `<li>${languages} </li>`).join('')
 }
 
+function updatePortfolio(profileData){
+    const portfolio = document.getElementById("profile.portfolio");
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `<li>
+                    <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+                    <a href="${project.url}" target="_blank">${project.url}</a>
+                </li>`
+    }).join('')
+}
+
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateCourses(profileData)
     updateAbility(profileData)
     updateLanguages(profileData)
+    updatePortfolio(profileData)
 })()
